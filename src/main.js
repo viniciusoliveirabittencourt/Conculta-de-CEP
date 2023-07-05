5fd678145ff6a76b38706e18333a378614314f76
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2023-07-05 20:22:47 -0300

    finalizando a lista com as característcas do cep

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-import './style.css';
+import './style.css'
+
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+const span = document.getElementsByClassName('info');
+
+button.addEventListener('click', handleClick);
+
+function creatSpan(data) {
+  const infoCep = Object.values(data);
+  for (let i = 0; i <= span.length; i += 1) {
+    span[i].innerHTML = infoCep[i];
+  }
+}
+
+async function handleClick() {
+  try {
+    const result = await fetch(`https://viacep.com.br/ws/${input.value}/json/`);
+    const data = await result.json();
+    creatSpan(data);
+
+    return data;
+  } catch (error) {
+    return error.message;
+  }
+}
